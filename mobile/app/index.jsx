8a8f61a441df0b468cc73e1fddafeafccc74c9c5
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2025-03-16 17:23:08 +0530

    add API URL constant and styles for various components

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello This is my first app</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello This is my first app</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "red",
+  },
+});
---
+++ mobile/constants/api.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://book-worm-react-native.onrender.com/api";
