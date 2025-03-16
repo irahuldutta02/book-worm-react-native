0f78bcce3d3924d35f48e76b681b306e8c9f24a4
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2025-03-16 16:53:09 +0530

    update app name and welcome message in index screen

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Hello This is my first app</Text>
     </View>
   );
 }
